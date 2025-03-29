695e3cf00c1cc598784500e4cf11aecd5b09dea4
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2025-03-29 20:37:45 +0300

    Delete lesson__11 directory

--- lesson__11/js/block-3.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", function () {
-    const block = document.querySelector(".main_center-3block");
-    const rect = block.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-    
-    if (rect.top < windowHeight && rect.bottom > 0) {
-        block.classList.add("show");
-    }
-});
