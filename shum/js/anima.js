23e8ee374281c8b87ed460849067193c6f735fab
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2025-08-21 13:04:29 +0300

    Add files via upload

---
+++ shum/js/anima.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const heroImg = document.querySelector('.hero-img');
+  const heroTitle = document.querySelector('.hero-block h1 span');
+  const heroText = document.querySelector('.hero-block p');
+
+});
