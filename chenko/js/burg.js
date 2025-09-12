58856c7c09969c9018a871ff0769df395fc2e3e5
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2025-09-12 17:27:20 +0300

    Delete chenko directory

--- chenko/js/burg.js
+++
@@ -1,8 +0,0 @@
-// Отримуємо елементи для мобільного меню
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-// Додаємо обробник подій для перемикання меню
-mobileMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
