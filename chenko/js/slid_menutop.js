541bec05c8806a7d27487562fe712f1a55376e76
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2025-09-17 01:03:27 +0300

    Update slid_menutop.js

--- chenko/js/slid_menutop.js
+++ chenko/js/slid_menutop.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sliderImage = document.getElementById('sliderImage');
   const images = [
-    '/img/main/1/2.jpg',
-    '/img/main/1/3.jpg', // Додай свої фото сюди
-    '/img/main/1/4.jpg',
-    '/img/main/1/5.jpg',
-    '/img/main/1/6.jpg',
-    '/img/main/1/7.jpg',
-    '/img/main/1/8.jpg',
-    '/img/main/1/1.jpg',
+    'img/main/1/2.jpg',
+    'img/main/1/3.jpg', // Додай свої фото сюди
+    'img/main/1/4.jpg',
+    'img/main/1/5.jpg',
+    'img/main/1/6.jpg',
+    'img/main/1/7.jpg',
+    'img/main/1/8.jpg',
+    'img/main/1/1.jpg',
   ];
   let currentImageIndex = 0;
   let interval;
@@ -28,4 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Початковий запуск для плавності (опціонально)
   changeImage();
+
 });
