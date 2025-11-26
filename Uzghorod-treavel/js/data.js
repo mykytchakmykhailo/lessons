728b21029011e239474005a45f9642d134c7d2a0
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2025-11-26 21:43:33 +0300

    Add files via upload

---
+++ Uzghorod-treavel/js/data.js
@@ -0,0 +1,13 @@
+// js/data.js — ТУТ ВСТАВЛЯЄШ СВОЇ СКУЛЬПТУРИ
+const sculptures = [
+	{ name: "Ліхтарник Дядя Коля", lat: 48.623000, lng: 22.299000, img: "img/figure/1.jpg" },
+	{ name: "Статуя Свободи", lat: 48.620800, lng: 22.287900, img: "img/figure/2.jpg" },
+	{ name: "Годинникар Шандор", lat: 48.621500, lng: 22.294100, img: "img/figure/3.jpg" },
+	{ name: "Моцарт", lat: 48.623100, lng: 22.302500, img: "img/figure/4.jpg" },
+	{ name: "Енді Воргол", lat: 48.620100, lng: 22.275400, img: "img/figure/5.jpg" },
+	{ name: "Джон Лорд", lat: 48.624000, lng: 22.300100, img: "img/figure/6.jpg" },
+	{ name: "Піноккіо", lat: 48.619800, lng: 22.298700, img: "img/figure/7.jpeg" },
+	{ name: "Гаррі Гудіні", lat: 48.621900, lng: 22.287500, img: "img/figure/8.jpg" },
+	{ name: "Лампочка Ілона Маска", lat: 48.622300, lng: 22.291200, img: "img/figure/9.jpg" },
+	// Додавай ще 44+ сюди — просто копіюй рядок і міняй назву/фото/координати
+];
