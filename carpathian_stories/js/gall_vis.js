dda5ce1c8c6cf663018914da9fb38ccb0394f9f7
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2026-01-02 19:16:16 +0300

    Delete carpathian_stories directory

--- carpathian_stories/js/gall_vis.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const galleryImages = document.querySelectorAll(".gall-grid img");
-
-  galleryImages.forEach((img, index) => {
-    setTimeout(() => {
-      img.classList.add("visible");
-    }, index * 250); // затримка між появою (150мс між кожним)
-  });
-});
