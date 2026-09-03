47953964407d5e946babd36001ef4f06a9a64c98
mykytchakmykhailo <121950695+mykytchakmykhailo@users.noreply.github.com>
2026-09-03 14:28:31 +0300

    Delete LUXUSNÍ RENOVACE directory

--- LUXUSNÍ RENOVACE/js/burg.js
+++
@@ -1,9 +0,0 @@
-  const mobileMenu = document.getElementById('mobileMenu');
-  const navLinks = document.querySelector('.nav-links');
-
-  if (mobileMenu) {
-    mobileMenu.addEventListener('click', () => {
-      mobileMenu.classList.toggle('active');
-      navLinks.classList.toggle('active');
-    });
-  }
--- LUXUSNÍ RENOVACE/js/top_text.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const text = document.querySelector('.overlay-text');
-    text.classList.add('show');
-    
-    setTimeout(() => {
-        text.classList.remove('show');
-    }, 3000); // Текст зникає через 3 секунди
-});
